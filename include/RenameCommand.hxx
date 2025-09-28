f00450386aa658fa8d8cadbbabaa735abba2bb53
Loaf <cookcheeseloaf@proton.me>
2025-09-28 20:18:11 +0300

    replacing tuple into optional with pair & adding quoted argument

--- include/RenameCommand.hxx
+++ include/RenameCommand.hxx
@@ -13,7 +13,8 @@ public:
     [[nodiscard]] std::unique_ptr<Command> clone() const override;
 
 private:
-    static std::tuple<bool, fs::path, fs::path> parse_args(const std::string& args);
+    static std::optional<std::pair<fs::path, fs::path>> parse_args(const std::string& args, std::string& err);
+    static std::vector<std::string> split_quoted_args(const std::string& s);
 };
 
 #endif
