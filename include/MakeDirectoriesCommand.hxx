8c7cdc6bd3246dd04dbd47fac9166e94edcd8595
Loaf <cookcheeseloaf@proton.me>
2025-10-01 10:32:23 +0300

    Adding its own argument handling.

--- include/MakeDirectoriesCommand.hxx
+++ include/MakeDirectoriesCommand.hxx
@@ -5,12 +5,19 @@
 #define SHELL_MAKE_DIRECTORIES_COMMAND_HXX
 #include "CommandShell.hxx"
 #include <memory>
+#include <optional>
+#include <vector>
 
 class MakeDirectoriesCommand final : public Command
 {
 public:
     void execute(std::string const& /* args */) override;
     [[nodiscard]] std::unique_ptr<Command> clone() const override;
+
+private:
+    using two_paths = std::pair<fs::path, fs::path>;
+    static std::optional<two_paths> parse_args(std::string const& /* args */, std::string& /* err */);
+    static std::vector<std::string> split_quoted_args(std::string const& /* args */);
 };
 
 
