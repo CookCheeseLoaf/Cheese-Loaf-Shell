ccf0fe61a061dff165a330ea0ad5de5f25359510
loaf <cookcheeseloaf@proton.me>
2025-10-10 15:57:47 +0300

    Initial commit

--- include/REPL.hxx
+++ include/REPL.hxx
@@ -36,8 +36,8 @@ public:
 
     static std::string get_dir();
     static constexpr int MAJOR = 0;
-    static constexpr int MINOR = 2;
-    static constexpr int PATCH = 1;
+    static constexpr int MINOR = 1;
+    static constexpr int PATCH = 0;
     ParsedCmd parsed_command;
 };
 
